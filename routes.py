c970b9ca4aec93199e2d5801f9dcc326ced04ac5
Sidharth Raguraman <ragurama@usc.edu>
2019-09-18 14:56:01 -0700

    adjusted routes file

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 
 import spotipy 
 import spotipy.util as util 
@@ -22,9 +22,18 @@ spotify = spotipy.Spotify(auth=token)
 @app.route('/')
 @app.route('/index')
 def index():
-    username = "127904618"
-    playlists = spotify.user_playlists(username)
-    playlist_names = []
-    for playlist in playlists['items']:
-        playlist_names.append(playlist['name'])
-    return render_template('index.html')
+    return render_template("index.html")
+
+@app.route('/new_playlist', methods=['POST'])
+def get_info():
+    new_playlist_name = request.form['new-playlist']
+    user_username = request.form['spotify-username']
+    guest_one_username = request.form['first-guest']
+    guest_two_username = request.form['second-guest']
+    top_forty = request.form['top-40-selection']
+
+    return render_template("new_playlist.html",  new_playlist_name=new_playlist_name)
+    
+
+
+
