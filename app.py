93a5c9590c8cfbdaa5d587524e0d0da76306f255
Sidharth Raguraman <ragurama@usc.edu>
2019-10-01 01:09:48 -0700

    added args.get

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def index():
 
 @app.route("/callback/q")
 def callback():
-    token = request.args['code']
+    token = request.args.get['code']
     payload = {
         "grant_type": "authorization_code",
         "code": str(token),
